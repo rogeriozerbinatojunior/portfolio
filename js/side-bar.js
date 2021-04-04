10b5fc899a5cbe45eabbcc0f9125ee6d40e2a613
Rogério Zerbinato Junior <rogeriojzerbinato@gmail.com>
2021-04-04 03:18:53 -0300

    Create sidebar and main-banner

---
+++ js/side-bar.js
@@ -0,0 +1,4 @@
+var threeLines = document.querySelector(".three-lines");
+threeLines.addEventListener('click', function() {
+    document.querySelector('.main-sidebar').classList.toggle("show-menu");
+});
