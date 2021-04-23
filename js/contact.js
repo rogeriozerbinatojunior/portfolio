4c0d9afe527ba3ec4b42fed1a07ed5b5360e0993
Rogério Zerbinato Junior <rogeriojzerbinato@gmail.com>
2021-04-23 00:14:08 -0300

    Variation main-projects and create contact-js

---
+++ js/contact.js
@@ -0,0 +1,4 @@
+var inputName = document.querySelector('#name');
+var inputEmail = document.querySelector('#email');
+var inputTxtarea = document.querySelector('#msg-txtarea');
+var btnSubmit = document.querySelector('#btnSubmit');
